538cf06acdc00f36d8e61b5f32cd1c9f14eb3bce
GalinSavov <lemonkiller34@gmail.com>
2024-06-24 14:56:43 +0200

    Added animations, animator controller , and unit rotation

--- Turn-Based-Strategy/Assets/Scripts/Unit.cs
+++ Turn-Based-Strategy/Assets/Scripts/Unit.cs
@@ -8,19 +8,32 @@ namespace Game.Units
 {
     public class Unit : MonoBehaviour
     {
+        [SerializeField] private Animator unitAnimator = null;
         private Vector3 targetPosition;
-        private float speed = 2f;
+        private float moveSpeed = 2f;
+        private float rotateSpeed = 10f;
         private float stoppingDistance = 0.1f;
 
         void Update()
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
-
+            
             if(Vector3.Distance(targetPosition, transform.position) > stoppingDistance)
-            transform.position += moveDirection * speed * Time.deltaTime;
+            {
+                transform.position += moveDirection * moveSpeed * Time.deltaTime;
+                transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+            }
+            else
+            unitAnimator.SetBool("isRunning",false);
 
             if (Mouse.current.leftButton.isPressed)
+            {
                 Move();
+                unitAnimator.SetBool("isRunning", true);
+            }
+
+            Debug.Log("Move direction: " + moveDirection);
+            Debug.Log("Target Position: " + targetPosition);
         }
 
         private void Move()

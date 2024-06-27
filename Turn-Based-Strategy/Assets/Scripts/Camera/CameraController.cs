4a27d7b8a630d0a99794ac0d6ae6e5182737a362
GalinSavov <lemonkiller34@gmail.com>
2024-06-27 14:47:00 +0200

    Added camera zoom

--- Turn-Based-Strategy/Assets/Scripts/Camera/CameraController.cs
+++ Turn-Based-Strategy/Assets/Scripts/Camera/CameraController.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-    
+    private CinemachineTransposer cinemachineTransposer;
+    private Vector3 followOffset;
     private TurnBasedStrategy inputActions;
+
+    private const float MIN_FOLLOW_OFFSET = 4f;
+    private const float MAX_FOLLOW_OFFSET = 12f;
+
     [SerializeField] private float cameraMoveSpeed = 2f;
     [SerializeField] private float cameraRotateSpeed = 2f;
+    [SerializeField] private float zoomSpeed = 2f;
+    [SerializeField] private CinemachineVirtualCamera cinemachineCamera = null;
     private void OnEnable()
     {
         inputActions.Enable();
@@ -17,6 +25,8 @@ public class CameraController : MonoBehaviour
     private void Awake()
     {
         inputActions = new TurnBasedStrategy();
+        cinemachineTransposer = cinemachineCamera.GetCinemachineComponent<CinemachineTransposer>();
+        followOffset = cinemachineTransposer.m_FollowOffset;
     }
 
     private void OnDisable()
@@ -25,18 +35,39 @@ public class CameraController : MonoBehaviour
     }
     void Update()
     {
+        MoveCamera();
+        RotateCamera();
+        ZoomCamera();
+    }
+
+    private void MoveCamera()
+    {
         Vector3 inputMoveDirection = Vector3.zero;
         inputMoveDirection += inputActions.Camera.CameraMove.ReadValue<Vector3>();
 
-        Vector3 moveVector = Vector3.forward * inputMoveDirection.z + Vector3.right * inputMoveDirection.x;
+        Vector3 moveVector = transform.forward * inputMoveDirection.z + transform.right * inputMoveDirection.x;
         transform.position += moveVector * cameraMoveSpeed * Time.deltaTime;
-
+    }
+    private void RotateCamera()
+    {
         Vector3 moveRotation = Vector3.zero;
         if (inputActions.Camera.CameraRotateLeft.IsPressed())
             moveRotation.y += 1f;
-        else if(inputActions.Camera.CameraRotateRight.IsPressed()) 
+        else if (inputActions.Camera.CameraRotateRight.IsPressed())
             moveRotation.y -= 1f;
 
         transform.rotation *= Quaternion.Euler(moveRotation * Time.deltaTime * cameraRotateSpeed);
+        
+    }
+    private void ZoomCamera()
+    {
+        if (Mouse.current.scroll.value.y > 0)
+            followOffset.y += zoomSpeed;
+
+        else if(Mouse.current.scroll.value.y < 0)
+            followOffset.y -= zoomSpeed;
+ 
+        followOffset.y = Mathf.Clamp(followOffset.y, MIN_FOLLOW_OFFSET, MAX_FOLLOW_OFFSET);
+        cinemachineTransposer.m_FollowOffset = Vector3.Lerp(cinemachineTransposer.m_FollowOffset, followOffset, Time.deltaTime * zoomSpeed);
     }
 }

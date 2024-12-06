7cb1e6b7f7405c81350920e1b429fcd34e80b8f7
GalinSavov <lemonkiller34@gmail.com>
2024-12-06 14:18:10 +0200

    Used ProBuilder to slice the crate when destroyed

--- Turn-Based-Strategy/Assets/Scripts/DestructableCrate.cs
+++ Turn-Based-Strategy/Assets/Scripts/DestructableCrate.cs
@@ -7,9 +7,23 @@ using UnityEngine;
 public class DestructableCrate : MonoBehaviour
 {
     public static Action<GridPosition> onAnyCrateDestroyed;
-   public void Damage()
+    [SerializeField] private Transform crateDestroyedPrefab = null;
+    public void Damage()
     {
-        onAnyCrateDestroyed?.Invoke(LevelGrid.Instance.GetGridPosition(transform.position));
+        Transform crateDestroyed = Instantiate(crateDestroyedPrefab,transform.position, transform.rotation);
+        ApplyForceToCrateParts(crateDestroyed, 150f, transform.position, 10f);
         Destroy(gameObject);
+        onAnyCrateDestroyed?.Invoke(LevelGrid.Instance.GetGridPosition(transform.position));
+    }
+    private void ApplyForceToCrateParts(Transform root,float explosionForce,Vector3 explosionPosition,float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if(child.TryGetComponent<Rigidbody>(out Rigidbody rb))
+            {
+                rb.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+            ApplyForceToCrateParts(child, explosionForce, explosionPosition, explosionRange);
+        }
     }
 }
